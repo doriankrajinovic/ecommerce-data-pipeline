d75bdd4781bb128598fc2b1932a39c05bde7de6e
Dorian Krajinović <krajinovic193@gmail.com>
2026-02-16 17:40:07 +0100

    Fix timestamp conversion: nanoseconds to microsecodns in staging models

--- dbt/ecommerce/models/staging/stg_customers.sql
+++ dbt/ecommerce/models/staging/stg_customers.sql
@@ -8,6 +8,6 @@ SELECT
     CONCAT(first_name, ' ', last_name) AS full_name,
     LOWER(email) AS email,
     country,
-    TIMESTAMP_MICROS(created_at) AS created_at,
+    TIMESTAMP_MICROS(CAST(created_at / 1000 AS INT64)) AS created_at,
     CURRENT_TIMESTAMP() AS loaded_at
 FROM {{ source('raw', 'customers') }}
